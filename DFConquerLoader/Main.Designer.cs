b1e15dbb0fef294ab5332525fab7452465b00f3a
DaRkFoxDeveloper <darkfoxdeveloper@gmail.com>
2019-09-28 18:35:19 +0200

    Base project working

---
+++ DFConquerLoader/Main.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace DFConquerLoader
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblHelp = new System.Windows.Forms.Label();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(22, 44);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(176, 44);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "PLAY";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "textBox1";
+            // 
+            // lblHelp
+            // 
+            this.lblHelp.AutoSize = true;
+            this.lblHelp.Location = new System.Drawing.Point(22, 15);
+            this.lblHelp.Name = "lblHelp";
+            this.lblHelp.Size = new System.Drawing.Size(176, 15);
+            this.lblHelp.TabIndex = 1;
+            this.lblHelp.Text = "Click on \"PLAY\" to launch game";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 102);
+            this.Controls.Add(this.lblHelp);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Main";
+            this.Text = "DFConquerLoader";
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblHelp;
+    }
+}
+
---
+++ DFConquerLoader/Main.cs
@@ -0,0 +1,42 @@
+﻿using Reloaded.Injector;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DFConquerLoader
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            Process pConquer = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = Directory.GetCurrentDirectory() + @"\Conquer.exe",
+                    Arguments = "blacknull"
+                }
+            };
+            if (File.Exists(pConquer.StartInfo.FileName))
+            {
+                pConquer.Start();
+                Injector injector = new Injector(pConquer);
+                injector.Inject("Inject.dll");
+                injector.Dispose();
+            } else
+            {
+                DialogResult dRes = MessageBox.Show("Error cannot load the Inject.dll for inject in Conquer.exe", "Error DFConquerLoader", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                if (dRes == DialogResult.Retry)
+                {
+                    this.Play_Click(sender, e);
+                }
+            }
+        }
+    }
+}
